1f5c7ac97d9be6c438c041ad5cf745953664d104
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-07-17 22:01:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,16 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-var lowercase = 'hello!'
 var uppercase = "HELLO!"
- if (lowercase.toLowerCase() === lowercase){
-  return "I can\'t hear you!"
- }else{
-  if (uppercase.toUpperCase() === uppercase){
+uppercase.toUpperCase() === uppercase
      return "YES INDEED!"
-   }
- }
 }
+   
 
 //var uppercase = "HELLO!"
 //uppercase.toUpperCase() === uppercase
